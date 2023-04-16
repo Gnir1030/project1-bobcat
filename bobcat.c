65197c728a6875093be7ef5f9ccf0b8bc05abc4a
Gnir1030 <78247475+Gnir1030@users.noreply.github.com>
2023-04-16 06:39:45 +0000

    make format

--- bobcat.c
+++ bobcat.c
@@ -6,57 +6,54 @@
 #include <string.h>
 #include <unistd.h>
 
-#define FILE_SIZE       1024
+#define FILE_SIZE 1024
 
-void readFile(char* buffer, char* readData, int fd, int* exitState){
-        int n = 0;
-        if( ( fd = open(buffer, O_RDONLY)) < 0){ // not exists files
-                warn("%s", buffer);
-                *exitState = 1;
-        }
-        else{
-                while( (n = read(fd, readData, FILE_SIZE)) != 0){
-                        if(n < 0){
-                                warn("%s", buffer);
-                                *exitState = 1;
-                                break;
-                        }
-                        write(1, readData, n);
-                }
-        }
+void readFile(char* buffer, char* readData, int fd, int* exitState) {
+  int n = 0;
+  if ((fd = open(buffer, O_RDONLY)) < 0) {  // not exists files
+    warn("%s", buffer);
+    *exitState = 1;
+  } else {
+    while ((n = read(fd, readData, FILE_SIZE)) != 0) {
+      if (n < 0) {
+        warn("%s", buffer);
+        *exitState = 1;
+        break;
+      }
+      write(1, readData, n);
+    }
+  }
 
-        close(fd);
+  close(fd);
 }
 
-void bobWrite(char* buffer){
-        int n = 0;
-        while(1){
-                if( (n = read(0, buffer, FILE_SIZE)) == 0){
-                        break;
-                }
-                write(1,  buffer, n);
-        }
+void bobWrite(char* buffer) {
+  int n = 0;
+  while (1) {
+    if ((n = read(0, buffer, FILE_SIZE)) == 0) {
+      break;
+    }
+    write(1, buffer, n);
+  }
 }
 
-int main(int argc, char * argv[]){
-        int fd = 0, exit = 0;
-        int* exitState = &exit;
-        char* readData = (char *)malloc(sizeof(char) * FILE_SIZE);
-        char* buffer = (char *)malloc(sizeof(char) * FILE_SIZE);
-        if(argc == 1)
-                bobWrite(buffer);
-        else if(argc > 1)
-        {
-                int i = 0;
-                while( ++i < argc){
-                        if(strcmp(argv[i], "-") == 0){
-                                bobWrite(buffer);
-                        }
-                        else
-                                readFile(argv[i], readData, fd, exitState);
-                }
-        }
-        free(readData);
-        free(buffer);
-        return exit;
+int main(int argc, char* argv[]) {
+  int fd = 0, exit = 0;
+  int* exitState = &exit;
+  char* readData = (char*)malloc(sizeof(char) * FILE_SIZE);
+  char* buffer = (char*)malloc(sizeof(char) * FILE_SIZE);
+  if (argc == 1)
+    bobWrite(buffer);
+  else if (argc > 1) {
+    int i = 0;
+    while (++i < argc) {
+      if (strcmp(argv[i], "-") == 0) {
+        bobWrite(buffer);
+      } else
+        readFile(argv[i], readData, fd, exitState);
+    }
+  }
+  free(readData);
+  free(buffer);
+  return exit;
 }
